99c51e7128980da66b7c36b6ebcbfa0a9fd01501
Chris Cummins <chrisc.101@gmail.com>
2021-02-12 19:20:27 +0000

    Generate enum declarations for LLVM capabilities.
    This adds new enums compiler_gym.envs.llvm.observation_spaces and
    compiler_gym.envs.llvm.reward_spaces that define the string names of
    the available LLVM observation and reward spaces.
    
    Fixes #72.

---
+++ compiler_gym/envs/llvm/make_specs.py
@@ -0,0 +1,34 @@
+"""Generate enum declarations for LLVM service capabilities.
+
+Usage: make_specs.py <service_binary> <output_path>.
+"""
+# TODO: As we add support for more compilers we could generalize this script
+# to work with other compiler services rather than hardcoding to LLVM.
+import sys
+from pathlib import Path
+
+from compiler_gym.envs.llvm.llvm_env import LlvmEnv
+
+
+def main(argv):
+    assert len(argv) == 3, "Usage: make_specs.py <service_binary> <output_path>"
+    service_path, output_path = argv[1:]
+
+    env = LlvmEnv(Path(service_path))
+    try:
+        with open(output_path, "w") as f:
+            print("from enum import Enum", file=f)
+            print(file=f)
+            print("class observation_spaces(Enum):", file=f)
+            for name in env.observation.spaces:
+                print(f'    {name} = "{name}"', file=f)
+            print(file=f)
+            print("class reward_spaces(Enum):", file=f)
+            for name in env.reward.spaces:
+                print(f'    {name} = "{name}"', file=f)
+    finally:
+        env.close()
+
+
+if __name__ == "__main__":
+    main(sys.argv)
