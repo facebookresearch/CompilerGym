77d23b746758729804d7284bfa8d4393efd3899a
Chris Cummins <cummins@fb.com>
2021-02-19 11:33:49 +0000

    Add license headers.

--- compiler_gym/service/__init__.py
+++ compiler_gym/service/__init__.py
@@ -1,3 +1,7 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 from compiler_gym.service.connection import (
     CompilerGymServiceConnection,
     ConnectionOpts,
--- compiler_gym/service/proto/__init__.py
+++ compiler_gym/service/proto/__init__.py
@@ -1,3 +1,7 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 from compiler_gym.service.proto.compiler_gym_service_pb2 import (
     ActionSpace,
     AddBenchmarkReply,
