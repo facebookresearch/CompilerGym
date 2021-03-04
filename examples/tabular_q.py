44ea8c01a30f5f36a854d1cfcfe6921d474c2349
Jiadong Guo <jdguo@fb.com>
2021-03-04 16:59:35 +0100

    format

--- examples/tabular_q.py
+++ examples/tabular_q.py
@@ -14,7 +14,6 @@ import random
 
 from absl import app, flags
 
-# from compiler_gym.envs import CompilerEnv
 from compiler_gym.util.flags.benchmark_from_flags import benchmark_from_flags
 from compiler_gym.util.flags.env_from_flags import env_from_flags
 
@@ -30,7 +29,7 @@ flags.DEFINE_list(
     "Indices of Alphaphase features that are used to construct a state",
 )
 flags.DEFINE_float("learning_rate", 0.1, "learning rate of the q-learning.")
-flags.DEFINE_int("episodes", 1000000, "number of episodes used to learn.")
+flags.DEFINE_integer("episodes", 1000, "number of episodes used to learn.")
 flags.DEFINE_float("epsilon", 0.2, "Epsilon rate of exploration. ")
 flags.DEFINE_integer("episode_length", 5, "The number of steps in each episode.")
 FLAGS = flags.FLAGS
@@ -38,7 +37,7 @@ FLAGS = flags.FLAGS
 
 def hash_state_action(autophase_feature, action):
     return tuple(
-        autophase_feature[FLAGS.features_indices] + [FLAGS.actions.index(action)]
+        (tuple(autophase_feature[FLAGS.features_indices]), FLAGS.actions.index(action))
     )
 
 
@@ -53,23 +52,76 @@ def select_action(q_table, ob, epsilon=0.0):
     return FLAGS.actions[qs.index(max(qs))]
 
 
+def get_max_q_value(q_table, ob):
+    max_q = 0
+    for act in FLAGS.actions:
+        hashed = hash_state_action(ob, act)
+        if hashed not in q_table:
+            q_table[hashed] = 0
+        max_q = max(q_table[hashed], max_q)
+    return max_q
+
+
 def train(q_table):
-    # TODO fill this function
-    return
+    # Buffer an old version of q table to inspect training progress
+    prev_q = {}
+
+    for i in range(FLAGS.episodes):
+        current_length = 0
+        env = get_env()
+        obs = env.reset()
+        while current_length < FLAGS.episode_length:
+            # Run Epsilon greedy policy.
+            a = select_action(q_table, obs, FLAGS.epsilon)
+            hashed = hash_state_action(obs, a)
+            if hashed not in q_table:
+                q_table[hashed] = 0
+
+            obs, reward, done, info = env.step(env.action_space.flags.index(a))
+            current_length += 1
+            # Get max q at new state.
+            target = reward + FLAGS.discount * get_max_q_value(q_table, obs)
+            # Update Q value at current state action pair.
+            q_table[hashed] = (
+                FLAGS.learning_rate * target
+                + (1 - FLAGS.learning_rate) * q_table[hashed]
+            )
+
+        if i % 100 == 0:
+            print(f"Running episode {i}, current Q table: ", q_table)
+
+            def compare_qs(q_old, q_new):
+                diff = [q_new[k] - v for k, v in q_old.items()]
+                return sum(diff) / len(diff) if diff else "NaN"
+
+            print(
+                f"Newly added Q entries {len(q_table)-len(prev_q)}, averaged diff {compare_qs(prev_q, q_table)}"
+            )
+            prev_q = q_table.copy()
+
+
+def setup_env():
+    FLAGS.observation = "Autophase"
+    FLAGS.reward = "IrInstructionCount"
+    FLAGS.benchmark = "cBench-v0/dijkstra"
+    FLAGS.env = "llvm-v0"
 
 
 def main(argv):
+    setup_env()
+    # Train a Q table.
     q_table = {}
     train(q_table)
+    # Rollout based on the Max-Q policy.
     env = get_env()
-    FLAGS.observation = "autophase"
     ob = env.reset()
     # Roll out one episode and report the resulting policy.
     action_seq, rewards = [], []
     for _ in range(FLAGS.episode_length):
         a = select_action(q_table, ob)
         action_seq.append(a)
-        ob, reward, done, info = env.step(env.action_space.index(a))
+        ob, reward, done, info = env.step(env.action_space.flags.index(a))
+
         rewards.append(reward)
     print("Resulting sequence: ", ",".join(action_seq), f"total reward{sum(rewards)}")
 
