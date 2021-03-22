1facf852f9ebd48e36bc39ca2f07c25351f29938
Chris Cummins <chrisc.101@gmail.com>
2021-03-22 11:13:18 +0000

    [tests] Remove tests that overwrite site data path.
    These no longer work now that site data requires LLVM binaries to be
    present.

--- tests/llvm/datasets_test.py
+++ tests/llvm/datasets_test.py
@@ -3,9 +3,6 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 """Tests for //compiler_gym/envs/llvm:legacy_datasets."""
-import os
-
-import gym
 import pytest
 
 from compiler_gym.envs.llvm import LlvmEnv, legacy_datasets
@@ -25,57 +22,6 @@ def test_validate_sha_output_okay():
 def test_validate_sha_output_invalid():
     output = legacy_datasets.BenchmarkExecutionResult(walltime_seconds=0, output="abcd")
     assert legacy_datasets.validate_sha_output(output)
-
-
-def test_default_cBench_dataset_require(tmpwd, temporary_environ):
-    """Test that cBench is downloaded."""
-    del temporary_environ
-
-    os.environ["COMPILER_GYM_SITE_DATA"] = str(tmpwd / "site_data")
-    env = gym.make("llvm-v0")
-    try:
-        assert not env.benchmarks, "Sanity check"
-
-        # Datasaet is downloaded.
-        assert env.require_dataset("cBench-v1")
-        assert env.benchmarks
-
-        # Dataset is already downloaded.
-        assert not env.require_dataset("cBench-v1")
-    finally:
-        env.close()
-
-
-def test_default_cBench_on_reset(tmpwd, temporary_environ):
-    """Test that cBench is downloaded by default when no benchmarks are available."""
-    del temporary_environ
-
-    os.environ["COMPILER_GYM_SITE_DATA"] = str(tmpwd / "site_data")
-    env = gym.make("llvm-v0")
-    try:
-        assert not env.benchmarks, "Sanity check"
-
-        env.reset()
-        assert env.benchmarks
-        assert env.benchmark.startswith("benchmark://cBench-v1/")
-    finally:
-        env.close()
-
-
-@pytest.mark.parametrize("benchmark_name", ["benchmark://npb-v0/1", "npb-v0/1"])
-def test_dataset_required(tmpwd, temporary_environ, benchmark_name):
-    """Test that the required dataset is downlaoded when a benchmark is specified."""
-    del temporary_environ
-
-    os.environ["COMPILER_GYM_SITE_DATA"] = str(tmpwd / "site_data")
-    env = gym.make("llvm-v0")
-    try:
-        env.reset(benchmark=benchmark_name)
-
-        assert env.benchmarks
-        assert env.benchmark.startswith("benchmark://npb-v0/")
-    finally:
-        env.close()
 
 
 def test_cBench_v0_deprecation(env: LlvmEnv):
