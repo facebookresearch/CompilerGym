95fb0773660f930826baab0011f6418212366316
Chris Cummins <cummins@fb.com>
2021-06-30 05:16:44 -0700

    Make commandline wrappers compatible with fork().
    Issue #291.

--- tests/wrappers/commandline_wrappers_test.py
+++ tests/wrappers/commandline_wrappers_test.py
@@ -10,25 +10,6 @@ from tests.test_main import main
 pytest_plugins = ["tests.pytest_plugins.llvm"]
 
 
-def test_constrained_action_space(env: LlvmEnv):
-    mem2reg_index = env.action_space["-mem2reg"]
-    reg2mem_index = env.action_space["-reg2mem"]
-
-    env = ConstrainedCommandline(env=env, flags=["-mem2reg", "-reg2mem"])
-
-    assert env.action_space.n == 2
-    assert env.action_space.flags == ["-mem2reg", "-reg2mem"]
-
-    assert env.action(0) == mem2reg_index
-    assert env.action([0, 1]) == [mem2reg_index, reg2mem_index]
-
-    env.reset()
-    env.step(0)
-    env.step([1, 1])
-
-    assert env.actions == [0, 1, 1]
-
-
 def test_commandline_with_terminal_action(env: LlvmEnv):
     mem2reg_unwrapped_index = env.action_space["-mem2reg"]
 
@@ -50,6 +31,64 @@ def test_commandline_with_terminal_action(env: LlvmEnv):
     _, _, done, info = env.step(0)
     assert done
     assert "terminal_action" in info
+
+
+def test_commandline_with_terminal_action_fork(env: LlvmEnv):
+    env = CommandlineWithTerminalAction(env)
+    assert env.unwrapped.action_space != env.action_space  # Sanity check.
+    fkd = env.fork()
+    try:
+        assert fkd.action_space == env.action_space
+
+        _, _, done, info = env.step(0)
+        assert done
+
+        _, _, done, info = fkd.step(0)
+        assert done
+    finally:
+        fkd.close()
+
+
+def test_constrained_action_space(env: LlvmEnv):
+    mem2reg_index = env.action_space["-mem2reg"]
+    reg2mem_index = env.action_space["-reg2mem"]
+
+    env = ConstrainedCommandline(env=env, flags=["-mem2reg", "-reg2mem"])
+
+    assert env.action_space.n == 2
+    assert env.action_space.flags == ["-mem2reg", "-reg2mem"]
+
+    assert env.action(0) == mem2reg_index
+    assert env.action([0, 1]) == [mem2reg_index, reg2mem_index]
+
+    env.reset()
+    env.step(0)
+    env.step([1, 1])
+
+    assert env.actions == [0, 1, 1]
+
+
+def test_constrained_action_space_fork(env: LlvmEnv):
+    mem2reg_index = env.action_space["-mem2reg"]
+    reg2mem_index = env.action_space["-reg2mem"]
+
+    env = ConstrainedCommandline(env=env, flags=["-mem2reg", "-reg2mem"])
+
+    fkd = env.fork()
+    try:
+        assert fkd.action_space.n == 2
+        assert fkd.action_space.flags == ["-mem2reg", "-reg2mem"]
+
+        assert fkd.action(0) == mem2reg_index
+        assert fkd.action([0, 1]) == [mem2reg_index, reg2mem_index]
+
+        fkd.reset()
+        fkd.step(0)
+        fkd.step([1, 1])
+
+        assert fkd.actions == [0, 1, 1]
+    finally:
+        fkd.close()
 
 
 if __name__ == "__main__":
