baf881e6f78cb9f404cc8318e89acff993a29db9
Chris Cummins <chrisc.101@gmail.com>
2021-09-09 11:59:22 +0100

    [gcc] Add tests using the system GCC compiler.

--- tests/gcc/gcc_bin_test.py
+++ tests/gcc/gcc_bin_test.py
@@ -9,6 +9,7 @@ import pytest
 import compiler_gym.envs.gcc  # noqa register environments
 from compiler_gym.service import ServiceError
 from tests.pytest_plugins.common import with_docker
+from tests.pytest_plugins.gcc import with_system_gcc, without_system_gcc
 from tests.test_main import main
 
 
@@ -26,6 +27,18 @@ def test_invalid_gcc_bin():
 def test_invalid_docker_image():
     with pytest.raises(ServiceError):
         gym.make("gcc-v0", gcc_bin="docker:not-a-valid-image")
+
+
+@with_system_gcc
+def test_system_gcc():
+    env = gym.make("gcc-v0", gcc_bin="gcc")
+    assert "gcc" in env.compiler_version
+
+
+@without_system_gcc
+def test_missing_system_gcc():
+    with pytest.raises(ServiceError):
+        gym.make("gcc-v0", gcc_bin="gcc")
 
 
 if __name__ == "__main__":
--- tests/pytest_plugins/gcc.py
+++ tests/pytest_plugins/gcc.py
@@ -4,6 +4,9 @@
 # LICENSE file in the root directory of this source tree.
 """Pytest fixtures for the GCC CompilerGym environments."""
 
+import subprocess
+from functools import lru_cache
+
 import gym
 import pytest
 
@@ -11,22 +14,53 @@ from compiler_gym.envs.gcc import GccEnv
 from tests.pytest_plugins.common import docker_is_available
 
 
-@pytest.fixture(scope="function")
-def env() -> GccEnv:
-    """Create a GCC environment."""
-    env = gym.make("gcc-v0")
+@lru_cache
+def system_gcc_is_available() -> bool:
+    """Return whether there is a system GCC available."""
     try:
-        yield env
-    finally:
-        env.close()
+        stdout = subprocess.check_output(
+            ["gcc", "--version"], universal_newlines=True, stderr=subprocess.DEVNULL
+        )
+        # On some systems "gcc" may alias to a different compiler, so check for
+        # the presence of the name "gcc" in the first line of output.
+        return "gcc" in stdout.split("\n")[0].lower()
+    except (subprocess.CalledProcessError, FileNotFoundError):
+        return False
+
+
+def gcc_environment_is_supported() -> bool:
+    """Return whether the requirements for the GCC environment are met."""
+    return docker_is_available() or system_gcc_is_available()
 
 
 # Decorator to skip a test if GCC environment is not supported.
 with_gcc_support = pytest.mark.skipif(
-    not docker_is_available(), reason="Docker is not available"
+    not gcc_environment_is_supported(), reason="Docker is not available"
 )
 
 # Decorator to skip a test if GCC environment is supported.
 without_gcc_support = pytest.mark.skipif(
-    docker_is_available(), reason="Docker is not available"
+    gcc_environment_is_supported(), reason="Docker is not available"
 )
+
+# Decorator to skip a test if system GCC is not availbale.
+with_system_gcc = pytest.mark.skipif(
+    not system_gcc_is_available(), reason="GCC is not available"
+)
+
+# Decorator to skip a test if system GCC is availbale.
+without_system_gcc = pytest.mark.skipif(
+    not system_gcc_is_available(), reason="GCC is available"
+)
+
+
+@pytest.fixture(scope="function")
+def env() -> GccEnv:
+    """Create a GCC environment."""
+    assert gcc_environment_is_supported(), "Cannot use GCC env fixture"
+
+    env_ = gym.make("gcc-v0", gcc_bin=None if docker_is_available() else "gcc")
+    try:
+        yield env_
+    finally:
+        env_.close()
