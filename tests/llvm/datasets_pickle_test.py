753c1fe887209e694e8a410ffbae12ef8ffc645c
Chris Cummins <cummins@fb.com>
2021-09-11 01:38:56 +0100

    [llvm] Skip dataset pickle tests on CI.

--- tests/llvm/datasets_pickle_test.py
+++ tests/llvm/datasets_pickle_test.py
@@ -9,17 +9,22 @@ import sys
 import pytest
 
 from compiler_gym.datasets import Dataset
+from tests.pytest_plugins.common import skip_on_ci
 from tests.test_main import main
 
 pytest_plugins = ["tests.pytest_plugins.llvm"]
 
 
+# Installing all datasets on CI is expensive.
+@skip_on_ci
 @pytest.mark.skipif(sys.version_info < (3, 7), reason="Fails on py3.6")
 def test_pickle_dataset(dataset: Dataset):
     """Test that datasets can be pickled."""
     assert pickle.loads(pickle.dumps(dataset)) == dataset
 
 
+# Installing all datasets on CI is expensive.
+@skip_on_ci
 def test_pickle_benchmark(dataset: Dataset):
     """Test that benchmarks can be pickled."""
     benchmark = next(dataset.benchmarks())
