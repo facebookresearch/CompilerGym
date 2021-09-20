4905a81f725cd5dfb47ceb0dec27d07aa42abf8f
Chris Cummins <cummins@fb.com>
2021-09-20 11:11:24 +0100

    Add missing header file.

--- examples/gcc_search.py
+++ examples/gcc_search.py
@@ -1,3 +1,7 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 import random
 from itertools import islice
 from typing import Callable, List, Optional, Tuple
