3460c24acc50f6d9035259f568e1756fc9961e39
Chris Cummins <chrisc.101@gmail.com>
2021-09-22 16:41:14 +0100

    [util] Add a shell_format.indent() function.

---
+++ tests/util/shell_format_test.py
@@ -0,0 +1,17 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+"""Unit tests for compiler_gym/util/shell_format.py"""
+from compiler_gym.util import shell_format as fmt
+from tests.test_main import main
+
+
+def test_indent():
+    assert fmt.indent("abc") == "    abc"
+    assert fmt.indent("abc", indent=2) == "  abc"
+    assert fmt.indent("abc\ndef") == "    abc\n    def"
+
+
+if __name__ == "__main__":
+    main()
