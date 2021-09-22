aafd4a100940c9eaa5c569f02451a0834e64eb8f
Chris Cummins <chrisc.101@gmail.com>
2021-09-22 11:33:48 +0100

    [tests] Fix csmith/anghabench tests when docker not available.

--- tests/gcc/datasets/anghabench_test.py
+++ tests/gcc/datasets/anghabench_test.py
@@ -10,7 +10,6 @@ import gym
 import pytest
 
 import compiler_gym.envs.gcc  # noqa register environments
-from compiler_gym.envs.gcc.datasets import AnghaBenchDataset
 from tests.pytest_plugins.common import skip_on_ci
 from tests.pytest_plugins.gcc import with_gcc_support
 from tests.test_main import main
@@ -18,46 +17,47 @@ from tests.test_main import main
 pytest_plugins = ["tests.pytest_plugins.common", "tests.pytest_plugins.gcc"]
 
 
-@pytest.fixture(scope="module")
-def anghabench_dataset() -> AnghaBenchDataset:
-    with gym.make("gcc-v0") as env:
-        ds = env.datasets["anghabench-v1"]
-    yield ds
+@with_gcc_support
+def test_anghabench_size(gcc_bin: str):
+    with gym.make("gcc-v0", gcc_bin=gcc_bin) as env:
+        anghabench_dataset = env.datasets["anghabench-v1"]
+
+        if sys.platform == "darwin":
+            assert anghabench_dataset.size == 1041265
+        else:
+            assert anghabench_dataset.size == 1041333
 
 
 @with_gcc_support
-def test_anghabench_size(anghabench_dataset: AnghaBenchDataset):
-    if sys.platform == "darwin":
-        assert anghabench_dataset.size == 1041265
-    else:
-        assert anghabench_dataset.size == 1041333
+def test_missing_benchmark_name(gcc_bin: str, mocker):
+    with gym.make("gcc-v0", gcc_bin=gcc_bin) as env:
+        anghabench_dataset = env.datasets["anghabench-v1"]
 
+        # Mock install() so that on CI it doesn't download and unpack the tarfile.
+        mocker.patch.object(anghabench_dataset, "install")
 
-@with_gcc_support
-def test_missing_benchmark_name(anghabench_dataset: AnghaBenchDataset, mocker):
-    # Mock install() so that on CI it doesn't download and unpack the tarfile.
-    mocker.patch.object(anghabench_dataset, "install")
+        with pytest.raises(
+            LookupError, match=r"^Benchmark not found: benchmark://anghabench-v1"
+        ):
+            anghabench_dataset.benchmark("benchmark://anghabench-v1")
+        anghabench_dataset.install.assert_called_once()
 
-    with pytest.raises(
-        LookupError, match=r"^Benchmark not found: benchmark://anghabench-v1"
-    ):
-        anghabench_dataset.benchmark("benchmark://anghabench-v1")
-    anghabench_dataset.install.assert_called_once()
-
-    with pytest.raises(
-        LookupError, match=r"^Benchmark not found: benchmark://anghabench-v1/"
-    ):
-        anghabench_dataset.benchmark("benchmark://anghabench-v1/")
-    assert anghabench_dataset.install.call_count == 2
+        with pytest.raises(
+            LookupError, match=r"^Benchmark not found: benchmark://anghabench-v1/"
+        ):
+            anghabench_dataset.benchmark("benchmark://anghabench-v1/")
+        assert anghabench_dataset.install.call_count == 2
 
 
 @with_gcc_support
 @skip_on_ci
 @pytest.mark.parametrize("index", range(10))
-def test_anghabench_random_select(anghabench_dataset: AnghaBenchDataset, index: int):
-    uri = next(islice(anghabench_dataset.benchmark_uris(), index, None))
-    benchmark = anghabench_dataset.benchmark(uri)
-    with gym.make("gcc-v0") as env:
+def test_anghabench_random_select(gcc_bin: str, index: int):
+    with gym.make("gcc-v0", gcc_bin=gcc_bin) as env:
+        anghabench_dataset = env.datasets["anghabench-v1"]
+
+        uri = next(islice(anghabench_dataset.benchmark_uris(), index, None))
+        benchmark = anghabench_dataset.benchmark(uri)
         env.reset(benchmark=benchmark)
 
 
--- tests/gcc/datasets/csmith_test.py
+++ tests/gcc/datasets/csmith_test.py
@@ -10,7 +10,7 @@ import gym
 import numpy as np
 import pytest
 
-from compiler_gym.envs.gcc.datasets import CsmithBenchmark, CsmithDataset
+from compiler_gym.envs.gcc.datasets import CsmithBenchmark
 from tests.pytest_plugins.common import is_ci
 from tests.pytest_plugins.gcc import with_gcc_support
 from tests.test_main import main
@@ -18,48 +18,54 @@ from tests.test_main import main
 pytest_plugins = ["tests.pytest_plugins.common", "tests.pytest_plugins.gcc"]
 
 
-@pytest.fixture(scope="module")
-def csmith_dataset() -> CsmithDataset:
-    with gym.make("gcc-v0") as env:
-        ds = env.datasets["generator://csmith-v0"]
-    yield ds
-
-
 @with_gcc_support
-def test_csmith_size(csmith_dataset: CsmithDataset):
-    assert csmith_dataset.size == 0
-    assert len(csmith_dataset) == 0
+def test_csmith_size(gcc_bin: str):
+    with gym.make("gcc-v0", gcc_bin=gcc_bin) as env:
+        csmith_dataset = env.datasets["generator://csmith-v0"]
+
+        assert csmith_dataset.size == 0
+        assert len(csmith_dataset) == 0
 
 
 @with_gcc_support
 @pytest.mark.parametrize("index", range(3) if is_ci() else range(10))
-def test_csmith_random_select(csmith_dataset: CsmithDataset, index: int, tmpwd: Path):
-    uri = next(islice(csmith_dataset.benchmark_uris(), index, None))
-    benchmark = csmith_dataset.benchmark(uri)
-    assert isinstance(benchmark, CsmithBenchmark)
-    with gym.make("gcc-v0") as env:
+def test_csmith_random_select(gcc_bin: str, index: int, tmpwd: Path):
+    with gym.make("gcc-v0", gcc_bin=gcc_bin) as env:
+        csmith_dataset = env.datasets["generator://csmith-v0"]
+
+        uri = next(islice(csmith_dataset.benchmark_uris(), index, None))
+        benchmark = csmith_dataset.benchmark(uri)
+        assert isinstance(benchmark, CsmithBenchmark)
         env.reset(benchmark=benchmark)
 
-    assert benchmark.source
-    benchmark.write_sources_to_directory(tmpwd)
-    assert (tmpwd / "source.c").is_file()
+        assert benchmark.source
+        benchmark.write_sources_to_directory(tmpwd)
+        assert (tmpwd / "source.c").is_file()
 
 
 @with_gcc_support
-def test_random_benchmark(csmith_dataset: CsmithDataset):
-    num_benchmarks = 5
-    rng = np.random.default_rng(0)
-    random_benchmarks = {
-        b.uri
-        for b in (csmith_dataset.random_benchmark(rng) for _ in range(num_benchmarks))
-    }
-    assert len(random_benchmarks) == num_benchmarks
+def test_random_benchmark(gcc_bin: str):
+    with gym.make("gcc-v0", gcc_bin=gcc_bin) as env:
+        csmith_dataset = env.datasets["generator://csmith-v0"]
+
+        num_benchmarks = 5
+        rng = np.random.default_rng(0)
+        random_benchmarks = {
+            b.uri
+            for b in (
+                csmith_dataset.random_benchmark(rng) for _ in range(num_benchmarks)
+            )
+        }
+        assert len(random_benchmarks) == num_benchmarks
 
 
 @with_gcc_support
-def test_csmith_from_seed_retry_count_exceeded(csmith_dataset: CsmithDataset):
-    with pytest.raises(OSError, match="Csmith failed after 5 attempts with seed 1"):
-        csmith_dataset.benchmark_from_seed(seed=1, max_retries=3, retry_count=5)
+def test_csmith_from_seed_retry_count_exceeded(gcc_bin: str):
+    with gym.make("gcc-v0", gcc_bin=gcc_bin) as env:
+        csmith_dataset = env.datasets["generator://csmith-v0"]
+
+        with pytest.raises(OSError, match="Csmith failed after 5 attempts with seed 1"):
+            csmith_dataset.benchmark_from_seed(seed=1, max_retries=3, retry_count=5)
 
 
 if __name__ == "__main__":
