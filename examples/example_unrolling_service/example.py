3db5091d98796d8807c6f9de7e1ae946eb7c42b1
Mostafa Elhoushi <melhoushi@fb.com>
2021-09-27 09:12:01 -0400

    try to create IR observation

--- examples/example_unrolling_service/example.py
+++ examples/example_unrolling_service/example.py
@@ -1,14 +1,24 @@
 import compiler_gym
-import examples.example_unrolling_service as unrolling_service
+import examples.example_unrolling_service as unrolling_service  # noqa Register environments.
+
+# temporary hack to avoid "isort" pre-hook error
+# TODO: remove this line
+print(unrolling_service.UNROLLING_PY_SERVICE_BINARY)
+
+# TODO: avoid using hard-coded paths for benchmark files
+# benchmark = compiler_gym.envs.llvm.llvm_env.make_benchmark("/Users/melhoushi/CompilerGym-Playground/dataset/offsets1.c")
+# benchmark = compiler_gym.datasets.benchmark.Benchmark.from_file("unrolling-example-for-now", "/Users/melhoushi/CompilerGym-Playground/dataset/offsets1.c")
+# benchmark = compiler_gym.datasets.benchmark.BenchmarkWithSource.create("unrolling-example-for-now", "/Users/melhoushi/CompilerGym-Playground/dataset/offsets1.c")
+benchmark = compiler_gym.envs.llvm.llvm_benchmark.make_benchmark(
+    "/Users/melhoushi/CompilerGym-Playground/dataset/offsets1.c"
+)
 
 env = compiler_gym.make(
     "unrolling-py-v0",
-    benchmark="unrolling-v0/foo",
+    benchmark=benchmark,  # "unrolling-v0/foo"
     observation_space="ir",
     reward_space="runtime",
 )
-
-print(unrolling_service.UNROLLING_PY_SERVICE_BINARY)
 
 observation = env.reset()
 
@@ -19,4 +29,5 @@ print("reward: ", reward)
 print("done: ", done)
 print("info: ", info)
 
+# TODO: implement write_bitcode(..)
 # env.write_bitcode("/tmp/output.bc")
--- examples/example_unrolling_service/service_py/example_service.py
+++ examples/example_unrolling_service/service_py/example_service.py
@@ -6,14 +6,18 @@
 # LICENSE file in the root directory of this source tree.
 """An example CompilerGym service in python."""
 import logging
+import subprocess
+import sys
 from pathlib import Path
-from typing import Optional, Tuple
+from signal import Signals
+from typing import List, Optional, Tuple
 
 from compiler_gym.service import CompilationSession
 from compiler_gym.service.proto import (
     Action,
     ActionSpace,
     Benchmark,
+    BenchmarkInitError,
     Observation,
     ObservationSpace,
     ScalarLimit,
@@ -84,6 +88,55 @@ class UnrollingCompilationSession(CompilationSession):
     ):
         super().__init__(working_directory, action_space, benchmark)
         logging.info("Started a compilation session for %s", benchmark.uri)
+        self._benchmark = benchmark
+        self._update_observations()
+
+    def _update_observations(self):
+        self._observation = dict()
+        # FIXME: "llvm-dis" path is not found. Should we add its path, or is there another way to obtain the IR?
+        ir = self._run_command(
+            ["llvm-dis", self._benchmark.program.uri, "/dev/stdout"], timeout=5
+        )
+        self._observation["ir"] = Observation(string_value=ir)
+
+        # TODO: update "features" and "runtime" observations
+
+    # _communicate(...) and _run_command(...) have been copied from llvm_benchmark.py
+    # TODO: avoid redundancies and reuse code instead of copying
+    def _communicate(self, process, input=None, timeout=None):
+        """subprocess.communicate() which kills subprocess on timeout."""
+        try:
+            return process.communicate(input=input, timeout=timeout)
+        except subprocess.TimeoutExpired:
+            # kill() was added in Python 3.7.
+            if sys.version_info >= (3, 7, 0):
+                process.kill()
+            else:
+                process.terminate()
+            raise
+
+    def _run_command(self, cmd: List[str], timeout: int):
+        process = subprocess.Popen(
+            cmd,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.PIPE,
+            universal_newlines=True,
+        )
+        stdout, stderr = self._communicate(process, timeout=timeout)
+        if process.returncode:
+            returncode = process.returncode
+            try:
+                # Try and decode the name of a signal. Signal returncodes
+                # are negative.
+                returncode = f"{returncode} ({Signals(abs(returncode)).name})"
+            except ValueError:
+                pass
+            raise BenchmarkInitError(
+                f"Compilation job failed with returncode {returncode}\n"
+                f"Command: {' '.join(cmd)}\n"
+                f"Stderr: {stderr.strip()}"
+            )
+        return stdout
 
     def apply_action(self, action: Action) -> Tuple[bool, Optional[ActionSpace], bool]:
         logging.info("Applied action %d", action.action)
@@ -94,7 +147,7 @@ class UnrollingCompilationSession(CompilationSession):
     def get_observation(self, observation_space: ObservationSpace) -> Observation:
         logging.info("Computing observation from space %s", observation_space)
         if observation_space.name == "ir":
-            return Observation(string_value="Hello, world!")
+            return self._observation["ir"]
         elif observation_space.name == "features":
             observation = Observation()
             observation.int64_list.value[:] = [0, 0, 0]
