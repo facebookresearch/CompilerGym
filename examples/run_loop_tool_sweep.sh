71596a9d88ff561d5ea6bc0aa37e3ddd4017e853
Bram Wasti <bwasti@fb.com>
2021-10-11 08:03:27 -0700

    check in code used to generate plots for loop_tool in paper

---
+++ examples/run_loop_tool_sweep.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -ve
+mkdir -p results
+
+python run_loop_tool_sweep.py  1 4 1 | tee results/s1_v4_log.txt
+python run_loop_tool_sweep.py  1 1 1 | tee results/s1_v1_log.txt
+
+python run_loop_tool_sweep.py  2 4 1 | tee results/s2_v4_log.txt
+python run_loop_tool_sweep.py  2 1 1 | tee results/s2_v1_log.txt
+
+python run_loop_tool_sweep.py  4 4 1 | tee results/s4_v4_log.txt
+python run_loop_tool_sweep.py  4 1 1 | tee results/s4_v1_log.txt
+
+python run_loop_tool_sweep.py  8 4 1 | tee results/s8_v4_log.txt
+python run_loop_tool_sweep.py  8 1 1 | tee results/s8_v1_log.txt
+
+python run_loop_tool_sweep.py  16 4 1 | tee results/s16_v4_log.txt
+python run_loop_tool_sweep.py  16 1 1 | tee results/s16_v1_log.txt
+
+python run_loop_tool_sweep.py  1 4 0 | tee results/s1_v4_linear.txt
+python run_loop_tool_sweep.py  1 1 0 | tee results/s1_v1_linear.txt
+
+python run_loop_tool_sweep.py  2 4 0 | tee results/s2_v4_linear.txt
+python run_loop_tool_sweep.py  2 1 0 | tee results/s2_v1_linear.txt
+
+python run_loop_tool_sweep.py  4 4 0 | tee results/s4_v4_linear.txt
+python run_loop_tool_sweep.py  4 1 0 | tee results/s4_v1_linear.txt
+
+python run_loop_tool_sweep.py  8 4 0 | tee results/s8_v4_linear.txt
+python run_loop_tool_sweep.py  8 1 0 | tee results/s8_v1_linear.txt
