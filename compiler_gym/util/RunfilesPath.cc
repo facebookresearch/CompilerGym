155b241606d2b8d01ef117b59f276613f3c2c857
Chris Cummins <chrisc.101@gmail.com>
2021-12-09 22:47:47 +0000

    [util] Add C++ implementation of getCacheRootPath().

--- compiler_gym/util/RunfilesPath.cc
+++ compiler_gym/util/RunfilesPath.cc
@@ -2,16 +2,24 @@
 //
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
+#include <fmt/format.h>
+
 #include "boost/filesystem.hpp"
 
 namespace fs = boost::filesystem;
 
 namespace compiler_gym::util {
 
-// When running under bazel, the working directory is the root of the
-// CompilerGym workspace. Back up one level so that we can reference other
-// workspaces.
-static const std::string kDefaultBase{"."};
+namespace {
+
+static const char* UNKNOWN_USER_NAME = "unknown";
+
+inline std::string getUser() {
+  const char* base = std::getenv("USER");
+  return base ? base : UNKNOWN_USER_NAME;
+}
+
+}  // namespace
 
 fs::path getRunfilesPath(const std::string& relPath) {
   const char* base = std::getenv("COMPILER_GYM_RUNFILES");
@@ -37,7 +45,26 @@ fs::path getSiteDataPath(const std::string& relPath) {
   } else {
     // $HOME may not be set under testing conditions. In this case, use a
     // throwaway directory.
-    return fs::temp_directory_path() / "compiler_gym" / relPath;
+    return fs::temp_directory_path() / fmt::format("compiler_gym_{}", getUser()) / relPath;
+  }
+}
+
+fs::path getCacheRootPath() {
+  // NOTE(cummins): This function has a related implementation in the Python
+  // sources, compiler_gym.util.runfiles_path.get_cache_path(). Any change to
+  // behavior here must be reflected in the Python version.
+  const char* force = std::getenv("COMPILER_GYM_CACHE");
+  if (force) {
+    return fs::path(force);
+  }
+
+  const char* home = std::getenv("HOME");
+  if (home) {
+    return fs::path(home) / ".local/cache/compiler_gym";
+  } else {
+    // $HOME may not be set under testing conditions. In this case, use a
+    // throwaway directory.
+    return fs::temp_directory_path() / fmt::format("compiler_gym_{}", getUser());
   }
 }
 
