070e21cc4e1f8a2d46405c519e76e64e9a8c1e8c
Chris Cummins <chrisc.101@gmail.com>
2022-02-18 02:53:04 +0000

    Fix typo in variable name and dependency.

--- compiler_gym/service/runtime/create_and_run_compiler_gym_service.py
+++ compiler_gym/service/runtime/create_and_run_compiler_gym_service.py
@@ -18,7 +18,7 @@ from typing import Type
 import grpc
 from absl import app, flags, logging
 
-from compiler_gym.service import connections
+from compiler_gym.service import connection
 from compiler_gym.service.compilation_session import CompilationSession
 from compiler_gym.service.proto import compiler_gym_service_pb2_grpc
 from compiler_gym.service.runtime.compiler_gym_service import CompilerGymService
@@ -95,7 +95,7 @@ def create_and_run_compiler_gym_service(
         # Create the service.
         server = grpc.server(
             futures.ThreadPoolExecutor(max_workers=FLAGS.rpc_service_threads),
-            options=connections.CLIENT_CONNECTION_OPTIONS,
+            options=connection.GRPC_CHANNEL_OPTIONS,
         )
         service = CompilerGymService(
             working_directory=working_dir,
