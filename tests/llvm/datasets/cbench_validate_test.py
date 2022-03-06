cd838bb48e1a8d8afcadc1c3da4daa55b65a0105
Chris Cummins <chrisc.101@gmail.com>
2022-03-06 22:20:09 +0000

    [tests] Increase LLVM datasets test timeout.

--- tests/llvm/datasets/cbench_validate_test.py
+++ tests/llvm/datasets/cbench_validate_test.py
@@ -3,6 +3,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 """Test for cBench semantics validation."""
+import pytest
+
 from compiler_gym import ValidationResult
 from compiler_gym.envs.llvm import LlvmEnv
 from tests.test_main import main
@@ -10,6 +12,7 @@ from tests.test_main import main
 pytest_plugins = ["tests.pytest_plugins.llvm"]
 
 
+@pytest.mark.timeout(600)
 def test_validate_benchmark_semantics(env: LlvmEnv, validatable_cbench_uri: str):
     """Run the validation routine on all benchmarks."""
     env.reward_space = "IrInstructionCount"
@@ -29,6 +32,7 @@ def test_validate_benchmark_semantics(env: LlvmEnv, validatable_cbench_uri: str)
     assert result.okay()
 
 
+@pytest.mark.timeout(600)
 def test_non_validatable_benchmark_validate(
     env: LlvmEnv, non_validatable_cbench_uri: str
 ):
