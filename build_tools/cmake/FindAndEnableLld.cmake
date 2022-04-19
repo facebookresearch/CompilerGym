4759c3191a86edf6fca12a3072efef4adab77fb5
Chris Cummins <cummins@fb.com>
2022-04-19 15:50:51 -0700

    [cmake] Don't override command line settings.

--- build_tools/cmake/FindAndEnableLld.cmake
+++ build_tools/cmake/FindAndEnableLld.cmake
@@ -2,6 +2,13 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
+if(
+    CMAKE_EXE_LINKER_FLAGS_INIT
+    OR CMAKE_MODULE_LINKER_FLAGS_INIT
+    OR CMAKE_SHARED_LINKER_FLAGS_INIT
+)
+    return()
+endif()
 
 find_program(LLD ld.lld)
 if(LLD)
