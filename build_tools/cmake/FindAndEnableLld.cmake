8204bf1d699c9d0bd42ac0d1272e93ec289d2c59
Boian Petkantchin <boian@nod-labs.com>
2022-05-05 14:04:38 -0700

    CMake: don't use *_INIT variables for setting lld linker

--- build_tools/cmake/FindAndEnableLld.cmake
+++ build_tools/cmake/FindAndEnableLld.cmake
@@ -6,15 +6,9 @@
 find_program(LLD ld.lld)
 if(LLD)
     message("Found lld: ${LLD}")
-    set(CMAKE_EXE_LINKER_FLAGS_INIT
-        "-fuse-ld=${LLD} ${CMAKE_EXE_LINKER_FLAGS_INIT}"
-    )
-    set(CMAKE_MODULE_LINKER_FLAGS_INIT
-        "-fuse-ld=${LLD} ${CMAKE_MODULE_LINKER_FLAGS_INIT}"
-    )
-    set(CMAKE_SHARED_LINKER_FLAGS_INIT
-        "-fuse-ld=${LLD} ${CMAKE_SHARED_LINKER_FLAGS_INIT}"
-    )
+    set(CMAKE_EXE_LINKER_FLAGS "-fuse-ld=${LLD} ${CMAKE_EXE_LINKER_FLAGS}")
+    set(CMAKE_MODULE_LINKER_FLAGS "-fuse-ld=${LLD} ${CMAKE_MODULE_LINKER_FLAGS}")
+    set(CMAKE_SHARED_LINKER_FLAGS "-fuse-ld=${LLD} ${CMAKE_SHARED_LINKER_FLAGS}")
 else()
     message("lld not found")
 endif()
