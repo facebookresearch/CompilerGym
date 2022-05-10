96ee4ca4fc881b2cdab7fde18a88f2542d174114
Chris Cummins <cummins@fb.com>
2022-05-10 08:37:59 -0700

    Format import list.

--- compiler_gym/util/gym_type_hints.py
+++ compiler_gym/util/gym_type_hints.py
@@ -2,8 +2,8 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-from typing import Any, Dict, List, Optional, Tuple, TypeVar, Union
 from enum import Enum
+from typing import Any, Dict, List, Optional, Tuple, TypeVar, Union
 
 # A JSON dictionary.
 JsonDictType = Dict[str, Any]
