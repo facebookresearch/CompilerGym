5580b36a5e21b1ecaf7b9d0e71c6b05298d7b047
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-06 19:17:29 +0000

    Adding tarfile member sanitization to extractall()

--- compiler_gym/envs/llvm/datasets/cbench.py
+++ compiler_gym/envs/llvm/datasets/cbench.py
@@ -242,7 +242,26 @@ def download_cBench_runtime_data() -> bool:
         tar_contents = io.BytesIO(download(url, sha256))
         with tarfile.open(fileobj=tar_contents, mode="r:bz2") as tar:
             cbench_data.parent.mkdir(parents=True, exist_ok=True)
-            tar.extractall(cbench_data.parent)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, cbench_data.parent)
         assert cbench_data.is_dir()
         # Create the marker file to indicate that the directory is unpacked
         # and ready to go.
